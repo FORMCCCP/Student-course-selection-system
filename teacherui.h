47e3aab65688abb2971af268183ed64815262b46
gaoge 2024051604053 <gg87891452@163.com>
2026-01-17 22:05:33 +0800

    加了教师的UI

--- teacherui.h
+++ teacherui.h
@@ -6,7 +6,8 @@ import std;
 
 using std::string;
 
-class TeacherUI{
+class TeacherUI
+{
 private:
     std::shared_ptr<pqxx::connection> connection;
 public:
@@ -21,11 +22,68 @@ public:
 TeacherUI::TeacherUI(std::shared_ptr<pqxx::connection> conn):
     connection(conn){}
 
+void TeacherUI::showTeacherMenu(Serteacher& tea)
+{
+    print("===教师界面===\n");
+    print("教师:{} 专业:{}\n",
+        tea.getTeacher()->getName(),
+        tea.getTeacher()->getMajor());
+    print("1,查看已教授课程\n2,评定成绩\n3,退出\n");
+}
 
-void Ser_teacher(){
-    Serteacher(connection);
+void TeacherUI::Ser_teacher()
+{
+    Serteacher tea(connection);
 
+    if(tea.login()){
+     print("登陆成功! 欢迎老师{}\n\n",tea.getTeacher()->getName());
+    }else{
+        print("登陆失败");
+        waitForEnter();
+        clearScreen();
+        return;
+    }
+    showTeacherMenu(tea);
 
+    while(1)
+    {
+        print("请选择功能:");
+        int choose;
+        std::cin >> choose;
+        switch(choose)
+        {
+        case 1:
+            print("\n以下是已教授的课程:\n");
+            print("========\n");
+            tea.viewTeachingCourses(tea.getTeacher()->getId());
+            print("\n");
+            waitForEnter();
+            clearScreen();
+            break;
+        case 2:
+            print("\n=========\n");
+            tea.evaluateGrade(tea.getTeacher()->getId());
+            print("=========\n");
+            waitForEnter();
+            clearScreen();
+            break;
+        case 3:
+            clearScreen();
+            return;
+        default:
+            print("无效选择，");
+            continue;
+        }
+    }
+}
 
+void TeacherUI::clearScreen() {
+    std::cout << "\033[2J\033[H" << std::flush;
+}
 
+void TeacherUI::waitForEnter() {
+    std::cout << "\n按回车键返回菜单...";
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin.get();
+    clearScreen();
 }
