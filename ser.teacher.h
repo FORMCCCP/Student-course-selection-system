f6a070ca07f31cc9d586fa612a385620b123f161
gaoge 2024051604053 <gg87891452@163.com>
2026-01-18 22:28:23 +0800

    添加了do.teacher和ser.teacher的内容

--- ser.teacher.h
+++ ser.teacher.h
@@ -28,3 +28,51 @@ public:
 
 Serteacher::Serteacher(std::shared_ptr<pqxx::connection> conn):
     Coubroker(conn), enbroker(conn), teabroker(conn){}
+
+bool Serteacher::login()
+{
+    string id, password;
+    print("输入ID:");
+    std::cin>>id;
+    print("输入密码:");
+    std::cin>>password;
+    if(Teabroker.handleLogin(id, password))
+    {
+        teacher = Teabroker.returnStudent(id);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+//获取教师对象
+class Teacher* Serteacher::getTeacher()
+{
+    return teacher;
+}
+
+//查看教授的课程
+void Serteacher::viewTeachingCourses(string id)
+{
+    teachingCourses(id);
+    if(teachingCourses.empty())
+    {
+        print("你没有要教授的课程\n");
+        print("=========\n");
+        return;
+    }
+    for(class Course* c: teachingCourses)
+    {
+        print("{}\n", c->getName());
+        print("课程ID:{}  学分:{}  容量:{}\n",c->getId(),c->getCredit(),c->getCapacity());
+        print("目前人数:{}  所授老师ID:{}\n",c->getCurrentCapacity(),c->getTeacherid());
+        print("开放学期:{}  开放年级:{}\n",c->getSession(),c->getGrade());
+        print("=========\n");
+    }
+}
+
+void Serteacher::evaluateGrade(string id)
+{
+    teabroker.evaluateGradeToStudent(c_id,id,Stubroker);
+}
